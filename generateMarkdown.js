1d02e314a90f17b8892033e0a2eb16a02eececf8
solomonkrow <metro317@live.com>
2023-10-13 15:30:24 -0500

    starter code

---
+++ generateMarkdown.js
@@ -0,0 +1,63 @@
+/* // TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {} */
+
+const generateMarkdown = (data) => {
+  return `
+  # ${data.title}
+
+  ## Description
+
+  ${data.description}
+  
+  ## Table of Contents
+   
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Badges](#badges)
+  - [Features](#features)
+  - [Contact](#contact)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Credits
+  
+  ${data.credits}
+  
+  ## License
+  
+  ${data.license}
+  
+  ## Badges
+    
+  ${data.badges}
+  
+  ## Features
+  
+  ${data.features}
+
+  ## Feedback / Contact
+
+  * Github : [${data.github}](https://www.github.com/${data.github})
+  * E-mail : ${data.email}
+  
+`;
+}
+
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,96 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const template = require('./generateMarkdown.js');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of the project?',
+            validate: (value) => value ? true : 'Input value required',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description of the project',
+            validate: (value) => value ? true : 'Input value required',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What steps are required to use project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions on how to use project',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List any collaborators / tutorials / 3rd party assets',
+        },
+        {
+            type: 'input',
+            name: 'badges',
+            message: 'Select any badges',
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'List any features',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select a license',
+            choices: ['MIT License','GNU General Public License v3.0','Apache License 2.0','N/a'],
+            validate: (value) => value ? true : 'Input value required',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your Github username?',
+            validate: (value) => value ? true : 'Input value required',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: (value) => value ? true : 'Input value required',
+        },
+    ])
+    /* .then(({
+        title,
+        description,
+        installation,
+        usage,
+        credits,
+        badges,
+        features,
+        license,
+        github,
+        email
+    })=>{
+        
+        template();
+        writeToFile(title,template);
+    }); */
+}; 
+
+function writeToFile(fileName, data) {
+    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`, data, (err)=>{
+        if(err){console.log(err);}
+        console.log('Your README has been generated');
+    })
+}
+
+function init() {
+    promptUser()
+        .then((data) => writeToFile('./files/README', template(data)))
+        .then(() => console.log('Successfully wrote to files folder'))
+        .catch((err) => console.error(err));
+};
+
+init();
